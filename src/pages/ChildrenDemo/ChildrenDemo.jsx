8989ecd34c6ba69269adb24b1922ddadf9a70bf0
bhatia-kunal <kunal.bhatia@successive.tech>
2019-02-26 13:31:30 +0530

    fix: 🐛 Added a dialog component to add Trainee

--- src/pages/ChildrenDemo/ChildrenDemo.jsx
+++ src/pages/ChildrenDemo/ChildrenDemo.jsx
@@ -6,60 +6,28 @@ const Children = () => (
     <Math first={7} second={4} operator="+">
       {(first, second, operator, result) => (
         <h3>
-          {first}
-          {' '}
-          {operator}
-          {' '}
-          {second}
-          {' '}
-          =
-          {' '}
-          {result}
+          {`${first} ${operator} ${second} = ${result}`}
         </h3>
       )}
     </Math>
     <Math first={7} second={3} operator="-">
       {(first, second, operator, result) => (
         <h3>
-          {first}
-          {' '}
-          {operator}
-          {' '}
-          {second}
-          {' '}
-          =
-          {' '}
-          {result}
+          {`${first} ${operator} ${second} = ${result}`}
         </h3>
       )}
     </Math>
     <Math first={7} second={0} operator="/">
       {(first, second, operator, result) => (
         <h3>
-          {first}
-          {' '}
-          {operator}
-          {' '}
-          {second}
-          {' '}
-          =
-          {' '}
-          {result}
+          {`${first} ${operator} ${second} = ${result}`}
         </h3>
       )}
     </Math>
     <Math first={7} second={3} operator="^">
       {(first, second, operator, result) => (
         <h3>
-          {first}
-          {' '}
-          {operator}
-          {' '}
-          {second}
-          {' '}
-          =
-          {' '}
-          {result}
+          {`${first} ${operator} ${second} = ${result}`}
         </h3>
       )}
     </Math>
