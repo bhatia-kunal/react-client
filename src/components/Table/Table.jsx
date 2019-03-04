83ecbe2a52c02234e71baa162b24d7bce4a90ca1
bhatia-kunal <kunal.bhatia@successive.tech>
2019-03-04 15:48:20 +0530

    feat: 🎸 Adding Sort, and onClick event to Table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
@@ -17,6 +18,12 @@ const styles = theme => ({
   table: {
     minWidth: 700,
   },
+  row: {
+    cursor: 'pointer',
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
 });
 
 const propTypes = {
@@ -24,6 +31,17 @@ const propTypes = {
   columns: PropTypes.arrayOf(PropTypes.objectOf).isRequired,
   data: PropTypes.arrayOf(PropTypes.objectOf).isRequired,
   classes: PropTypes.objectOf(PropTypes.string).isRequired,
+  orderBy: PropTypes.string,
+  order: PropTypes.string,
+  onSort: PropTypes.func,
+  onSelect: PropTypes.func,
+};
+
+const defaultProps = {
+  orderBy: '',
+  order: 'asc',
+  onSort: null,
+  onSelect: null,
 };
 
 function SimpleTable(props) {
@@ -32,27 +50,43 @@ function SimpleTable(props) {
     columns,
     data,
     id,
+    orderBy,
+    order,
+    onSort,
+    onSelect,
   } = props;
+
+  console.log(orderBy, order, onSort, onSelect, props);
 
   return (
     <Paper className={classes.root}>
-      <Table id={id} className={classes.table}>
+      <Table className={classes.table}>
         <TableHead>
           <TableRow>
             {
               columns.map((column => (
                 <TableCell key={column.field} align={column.align}>
-                  {column.label || column.field}
+                  <TableSortLabel
+                    active={orderBy === column.field}
+                    direction={order}
+                    onClick={onSort(column.field)}
+                  >
+                    {column.label || column.field}
+                  </TableSortLabel>
                 </TableCell>
               )))
             }
           </TableRow>
         </TableHead>
-        <TableBody>
-          {data.map(column => (
-            <TableRow key={column.id}>
+        <TableBody stripedRows>
+          {data.map(row => (
+            <TableRow className={classes.row} key={row[id]} hover onClick={() => onSelect(row)}>
               {columns.map(cell => (
-                (<TableCell key={cell.field} align={cell.align}>{column[cell.field]}</TableCell>)
+                (
+                  <TableCell key={cell.field} align={cell.align}>
+                    {(cell.format && cell.format(row[cell.field])) || row[cell.field]}
+                  </TableCell>
+                )
               ))}
             </TableRow>
           ))
@@ -64,5 +98,6 @@ function SimpleTable(props) {
 }
 
 SimpleTable.propTypes = propTypes;
+SimpleTable.defaultProps = defaultProps;
 
 export default withStyles(styles)(SimpleTable);
--- src/pages/Trainee/TraineeList.jsx
+++ src/pages/Trainee/TraineeList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import { AddDialog } from './components';
 import trainees from './data/trainee';
 import { Table } from '../../components';
+import { getDateFormatted } from '../../libs/utils';
 
 const style = {
   button: {
@@ -18,6 +19,8 @@ class TraineeList extends React.Component {
     name: '',
     email: '',
     password: '',
+    order: 'asc',
+    orderBy: '',
   };
 
   handleClickOpen = () => {
@@ -56,25 +59,26 @@ class TraineeList extends React.Component {
     );
   }
 
-  renderList = (listItems) => {
-    if (!listItems) {
-      return null;
-    }
-
-    return (
-      <ul>
-        {listItems.map(item => (
-          <li key={item.id}>
-            <Link to={`/trainee/${item.id}`} style={{ textDecoration: 'none' }}>
-              {item.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    );
+  handleOnSelect = (row) => {
+    const { history } = this.props;
+    history.push(`/trainee/${row.id}`);
   }
 
+  createSortHandler = property => event => this.handleRequestSort(event, property);
+
+  handleRequestSort = (event, property) => {
+    const currentOrderBy = property;
+    let currentOrder = 'asc';
+    const { orderBy, order } = this.state;
+    if (orderBy === property && order === 'asc') {
+      currentOrder = 'desc';
+    }
+
+    this.setState({ order: currentOrder, orderBy: currentOrderBy });
+  };
+
   render() {
+    const { order, orderBy } = this.state;
     console.log(this.state);
     return (
       <div>
@@ -94,14 +98,28 @@ class TraineeList extends React.Component {
             {
               field: 'email',
               label: 'Email Address',
+              format: value => value && value.toUpperCase(),
+            },
+            {
+              field: 'createdAt',
+              label: 'Date',
+              align: 'right',
+              format: getDateFormatted,
             },
           ]}
+          order={order}
+          orderBy={orderBy}
+          onSelect={this.handleOnSelect}
+          onSort={this.createSortHandler}
         />
         {this.renderDialog()}
-        {this.renderList(trainees)}
       </div>
     );
   }
 }
+
+TraineeList.propTypes = {
+  history: PropTypes.arrayOf.isRequired,
+};
 
 export default TraineeList;
