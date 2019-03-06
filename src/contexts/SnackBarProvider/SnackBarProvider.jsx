7ee7037ac93d6eefe37bba3e5981035ca5e3407b
bhatia-kunal <kunal.bhatia@successive.tech>
2019-03-06 17:26:53 +0530

    feat: 🎸 SnackBar and Context API

---
+++ src/contexts/SnackBarProvider/SnackBarProvider.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import CloseIcon from '@material-ui/icons/Close';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './style';
+
+const SnackBarContext = React.createContext(() => console.log('SnackBar context'));
+
+class SnackBarProvider extends Component {
+  variantIcon = {
+    success: CheckCircleIcon,
+    error: ErrorIcon,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      status: '',
+      message: '',
+    };
+  }
+
+  handleOpen = (message, status) => {
+    this.setState({
+      open: true,
+      message,
+      status,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false,
+      message: '',
+    });
+  };
+
+  render() {
+    const {
+      classes,
+      children,
+      ...other
+    } = this.props;
+    const { status, message, open } = this.state;
+    const Icon = this.variantIcon[status];
+    return (
+      <SnackBarContext.Provider value={this.handleOpen}>
+        {children}
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={open}
+          autoHideDuration={4000}
+        >
+          <SnackbarContent
+            className={classNames(classes[status])}
+            aria-describedby="client-snackbar"
+            message={
+              (
+                <span id="client-snackbar" className={classes.message}>
+                  <Icon className={classNames(classes.icon, classes.iconVariant)} />
+                  {message}
+                </span>
+              )
+            }
+            action={[
+              <IconButton
+                key="close"
+                aria-label="Close"
+                color="inherit"
+                className={classes.close}
+                onClick={this.handleClose}
+              >
+                <CloseIcon className={classes.icon} />
+              </IconButton>,
+            ]}
+            {...other}
+          />
+        </Snackbar>
+      </SnackBarContext.Provider>
+    );
+  }
+}
+
+SnackBarProvider.propTypes = {
+  classes: PropTypes.objectOf.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export const SnackBarConsumer = SnackBarContext.Consumer;
+
+export default withStyles(styles)(SnackBarProvider);
---
+++ src/contexts/SnackBarProvider/style.js
@@ -0,0 +1,26 @@
+import green from '@material-ui/core/colors/green';
+
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit,
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
+export default styles;
